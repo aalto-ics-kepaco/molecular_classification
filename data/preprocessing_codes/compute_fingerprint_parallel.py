10afcc7aa61a5a1cc280a4b0ef7775da81d1c257
hongyusu <hongyu.su.uh@gmail.com>
2015-04-02 11:30:48 +0300

    more information in readme

--- data/preprocessing_codes/compute_fingerprint_parallel.py
+++ data/preprocessing_codes/compute_fingerprint_parallel.py
@@ -1,5 +1,8 @@
 
 
+# The script enables parallel computation on interactive cluster (a.k.a. UKKO cluster of University of Helsinki). 
+# In other words, the script implements a  simiple abatch system for deploying computation jobs. Sbatch system is used in e.g. Triton cluster of Aalto University.
+
 
 
 #!/usr/bin/env python
@@ -44,10 +47,16 @@ class Worker(Thread):
 
 def single_thread(node,job):
   (job_id,job_content) = job
-  logging.info(" %s->%s" % (node, job_id))
+  logging.info(" %s->%s:%s" % (node, job_id, job_content))
   try:
-    #print("ssh -o StrictHostKeyChecking=no %s '/cs/fs/home/su/softwares/openbabel/bin/babel /cs/taatto/group/urenzyme/workspace/molecular_classification/data/structures/sdffiles/%s.sdf -ofpt -xfFP2 > /cs/taatto/group/urenzyme/workspace/molecular_classification/data/structures/FPfiles/%s.fp'" % (node,job_content,job_content))
-    singleres = commands.getoutput("ssh -o StrictHostKeyChecking=no %s '/cs/fs/home/su/softwares/openbabel/bin/babel /cs/taatto/group/urenzyme/workspace/molecular_classification/data/structures/sdffiles/%s.sdf -ofpt -xfFP4 > /cs/taatto/group/urenzyme/workspace/molecular_classification/data/structures/FPfiles/%s.fp4'" % (node,job_content,job_content))
+    if fp_flag == 1:
+      singleres = commands.getoutput("ssh -o StrictHostKeyChecking=no %s '/cs/fs/home/su/softwares/openbabel/bin/babel /cs/taatto/group/urenzyme/workspace/molecular_classification/data/structures/sdffiles/%s.sdf -ofpt -xfFP2 > /cs/taatto/group/urenzyme/workspace/molecular_classification/data/structures/FPfiles/%s.fp'" % (node,job_content,job_content))
+    elif fp_flag == 2:
+      singleres = commands.getoutput("ssh -o StrictHostKeyChecking=no %s '/cs/fs/home/su/softwares/openbabel/bin/babel /cs/taatto/group/urenzyme/workspace/molecular_classification/data/structures/sdffiles/%s.sdf -ofpt -xfFP3 > /cs/taatto/group/urenzyme/workspace/molecular_classification/data/structures/FPfiles/%s.fp3'" % (node,job_content,job_content))
+    elif fp_flag == 3:
+      singleres = commands.getoutput("ssh -o StrictHostKeyChecking=no %s '/cs/fs/home/su/softwares/openbabel/bin/babel /cs/taatto/group/urenzyme/workspace/molecular_classification/data/structures/sdffiles/%s.sdf -ofpt -xfFP4 > /cs/taatto/group/urenzyme/workspace/molecular_classification/data/structures/FPfiles/%s.fp4'" % (node,job_content,job_content))
+    else:
+      singleres = commands.getoutput("""ssh -o StrictHostKeyChecking=no %s 'cat /cs/taatto/group/urenzyme/workspace/molecular_classification/data/preprocessing_codes/run_sdf_to_mat.r | R --slave --args "/cs/taatto/group/urenzyme/workspace/molecular_classification/data/structures/sdffiles/%s.sdf" "/cs/taatto/group/urenzyme/workspace/molecular_classification/data/structures/MATLABfiles/%s.mat" ' """ % (node,job_content,job_content))
     logging.info(" %s-|%s" % (node, job_id))
   except:
     logging.info(" %s-<%s" % (node, job_id))
@@ -64,7 +73,14 @@ def compute_fingerprints_in_parallel():
   fin = open('../DTPNCI2015/results/ncicancer_labels')
   for line in fin:
     moleculelist.append(line.strip())
-  fppath = '../structures/FPfiles/'
+  if fp_flag==1:
+    fppath = '../structures/FPfiles/'
+  elif fp_flag==2:
+    fppath = '../structures/FPfiles/'
+  elif fp_flag==3:
+    fppath = '../structures/FPfiles/'
+  else:
+    fppath = '../structures/MATLABfiles/'
   if not os.path.exists(fppath):
     os.makedirs(fppath)
   logging.info("Generate jobs ...")
@@ -72,7 +88,15 @@ def compute_fingerprints_in_parallel():
   job_id = 0
   job_size = 1
   for molecule in moleculelist:
-    if os.path.exists("%s%s.fp4" % (fppath,molecule)):
+    if fp_flag==1:
+      sdffilename = "%s%s.fp" % (fppath,molecule)
+    elif fp_flag==2:
+      sdffilename = "%s%s.fp3" % (fppath,molecule)
+    elif fp_flag==3:
+      sdffilename = "%s%s.fp4" % (fppath,molecule)
+    else:
+      sdffilename = "%s%s.mat" % (fppath,molecule)
+    if os.path.exists(sdffilename):
       continue
     job_id = job_id + 1
     job_content=molecule
@@ -102,8 +126,20 @@ def compute_fingerprints_in_parallel():
 
 
 
-
-
-
+# fp_flag indicates the type of fingerprint computation
+# 1: fp2 fingerprint of Openbabel for computing Tanimoto kernel
+# 2: fp3 fingerprint of Openbabel for computing Tanimoto kernel
+# 3: fp4 fingerprint of Openbabel for computing Tanimoto kernel
+# 4: covert a sdf file into a adj matrix for computing other graph kernels 
+fp_flag = 3 
 compute_fingerprints_in_parallel()
+
+
+
+
+
+
+
+
+
 
